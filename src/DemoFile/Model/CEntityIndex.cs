fd84db419375ca7402c693aabb295e7cfd6251ce
Saul Rennison <saul.rennison@gmail.com>
2023-10-28 14:24:20 +0100

    use IsValid

--- src/DemoFile/Model/CEntityIndex.cs
+++ src/DemoFile/Model/CEntityIndex.cs
@@ -5,5 +5,5 @@ public readonly record struct CEntityIndex(uint Value)
     public static readonly CEntityIndex Invalid = new(unchecked((uint)-1));
     public bool IsValid => this != Invalid;
 
-    public override string ToString() => this == Invalid ? $"<invalid>" : $"Entity index {Value}";
+    public override string ToString() => IsValid ? $"Entity #{Value}" : "<invalid>";
 }
