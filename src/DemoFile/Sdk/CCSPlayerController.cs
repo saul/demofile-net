0663d286d98b2817544b497a9484d91c123bb2c4
Saul Rennison <saul.rennison@gmail.com>
2023-11-12 08:45:27 +0000

    More helpers

--- src/DemoFile/Sdk/CCSPlayerController.cs
+++ src/DemoFile/Sdk/CCSPlayerController.cs
@@ -7,6 +7,4 @@ public partial class CCSPlayerController
     public override string ToString() => $"{(IsActive ? "" : "[INACTIVE] ")}{PlayerName} ({Connected})";
 
     public string? PawnCharacterName => GameItems.ItemDefinitions.GetValueOrDefault(PawnCharacterDefIndex)?.Name;
-
-    public new CCSPlayerPawn? Pawn => (CCSPlayerPawn?) base.Pawn;
 }
