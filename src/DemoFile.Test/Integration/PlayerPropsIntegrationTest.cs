1b902f44a2618dca2dd114afaee12700ce3234a2
Saul Rennison <saul.rennison@gmail.com>
2023-11-25 14:11:32 +0000

    Add snapshot tests

---
+++ src/DemoFile.Test/Integration/PlayerPropsIntegrationTest.cs
@@ -0,0 +1,92 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace DemoFile.Test.Integration;
+
+[TestFixture]
+public class PlayerPropsIntegrationTest
+{
+    [Test]
+    public async Task Position()
+    {
+        // Arrange
+        var snapshot = new StringBuilder();
+        var demo = new DemoParser();
+
+        demo.PacketEvents.SvcServerInfo += e =>
+        {
+            snapshot.AppendLine($"[{demo.CurrentGameTick.Value}] {JsonSerializer.Serialize(e, DemoJson.SerializerOptions)}");
+        };
+
+        demo.Source1GameEvents.RoundEnd += e =>
+        {
+            snapshot.AppendLine($"[{demo.CurrentGameTick.Value}] Round end snapshot:");
+            SnapshotPlayerState();
+        };
+
+        void SnapshotPlayerState()
+        {
+            foreach (var player in demo.Players)
+            {
+                var pawn = player.PlayerPawn;
+
+                var weapons = pawn?.Weapons.Select(wep => new
+                {
+                    Class = wep.ServerClass.Name,
+                    Weapon = wep.EconItem.Name,
+                    PaintKit = wep.EconItem.PaintKit,
+                    Quality = wep.EconItem.Quality,
+                    Rarity = wep.EconItem.Rarity,
+                    CustomName = wep.EconItem.CustomName,
+                    Clip1 = wep.Clip1,
+                    Clip2 = wep.Clip2,
+                    Reserve = wep.ReserveAmmo,
+                });
+
+                var playerJson = JsonSerializer.Serialize(
+                        new
+                        {
+                            player.IsActive,
+                            PlayerName = player.PlayerName,
+                            Team = player.Team.ToString(),
+                            IsAlive = pawn?.IsAlive,
+                            ControllerHandle = player.EntityHandle,
+                            PawnHandle = player.PawnHandle,
+                            Kills = player.ActionTrackingServices?.MatchStats.Kills,
+                            Deaths = player.ActionTrackingServices?.MatchStats.Deaths,
+                            LastPlaceName = pawn?.LastPlaceName,
+                            ActiveWeapon = pawn?.ActiveWeapon?.ServerClass.Name,
+                            Origin = pawn?.Origin,
+                            Rotation = pawn?.Rotation,
+                            EyeAngles = pawn?.EyeAngles,
+                            Weapons = weapons
+                        },
+                        DemoJson.SerializerOptions)
+                    .ReplaceLineEndings(Environment.NewLine + "  ");
+
+                snapshot.AppendLine($"  {playerJson}");
+            }
+        }
+
+        var playerSnapshotInterval = TimeSpan.FromMinutes(10);
+        void OnSnapshotTimer()
+        {
+            snapshot.AppendLine($"[{demo.CurrentGameTick.Value}] Interval snapshot:");
+            SnapshotPlayerState();
+
+            demo.CreateTimer(
+                demo.CurrentDemoTick + playerSnapshotInterval,
+                OnSnapshotTimer);
+        }
+
+        demo.CreateTimer(
+            demo.CurrentDemoTick + playerSnapshotInterval,
+            OnSnapshotTimer);
+
+        // Act
+        await demo.Start(GotvCompetitiveProtocol13963, default);
+
+        // Assert
+        Snapshot.Assert(snapshot.ToString());
+    }
+}
---
+++ src/DemoFile.Test/Snapshot.cs
@@ -0,0 +1,47 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace DemoFile.Test;
+
+public static class Snapshot
+{
+    private static readonly DirectoryInfo SnapshotDir = new(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Snapshots"));
+    private static readonly DirectoryInfo SnapshotSourceDir = FindSnapshotSourceDir();
+
+    private static DirectoryInfo FindSnapshotSourceDir()
+    {
+        var currentDir = new DirectoryInfo(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ".."));
+
+        while (currentDir != null)
+        {
+            var searchDir = new DirectoryInfo(Path.Combine(currentDir.FullName, "Snapshots"));
+            if (searchDir.Exists)
+            {
+                return searchDir;
+            }
+
+            currentDir = currentDir.Parent;
+        }
+
+        throw new Exception("Could not find 'Snapshots' source directory");
+    }
+
+    public static void Assert(string contents, [CallerMemberName] string? name = null)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+
+        var fileName = name + ".txt";
+        var path = Path.Combine(SnapshotDir.FullName, fileName);
+
+        if (File.Exists(path))
+        {
+            Console.WriteLine($"Comparing snapshot: {path}");
+            var expected = File.ReadAllText(path).ReplaceLineEndings();
+            NUnit.Framework.Assert.That(contents, Is.EqualTo(expected), "Snapshot mismatch");
+            return;
+        }
+
+        var outputPath = Path.Combine(SnapshotSourceDir.FullName, fileName);
+        Console.WriteLine($"Writing snapshot: {outputPath}");
+        File.WriteAllText(outputPath, contents.ReplaceLineEndings());
+    }
+}
