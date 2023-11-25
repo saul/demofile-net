f159cca8c537e681bec80f0f13cc177c34001f6a
Saul Rennison <saul.rennison@gmail.com>
2023-11-25 14:29:37 +0000

    Add string table snapshot

---
+++ src/DemoFile.Test/Integration/StringTableIntegrationTest.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Text.Json;
+using DemoFile.Sdk;
+
+namespace DemoFile.Test.Integration;
+
+[TestFixture]
+public class StringTableIntegrationTest
+{
+    [Test]
+    public async Task PlayerInfo()
+    {
+        // Arrange
+        var snapshot = new StringBuilder();
+        var demo = new DemoParser();
+
+        void SnapshotPlayerInfos()
+        {
+            var playerInfos = demo.PlayerInfos.Reverse().SkipWhile(x => x == null).Reverse().ToList();
+            for (var index = 0; index < playerInfos.Count; index++)
+            {
+                var playerInfo = demo.PlayerInfos[index];
+                snapshot.AppendLine($"  #{index}: {playerInfo?.ToString() ?? "<null>"}");
+
+                var controllerIndex = new CEntityIndex((uint)(index + 1));
+                Debug.Assert(ReferenceEquals(
+                    demo.GetEntityByIndex<CCSPlayerController>(controllerIndex)?.PlayerInfo,
+                    playerInfo));
+            }
+        }
+
+        var playerSnapshotInterval = TimeSpan.FromMinutes(1);
+        void OnSnapshotTimer()
+        {
+            snapshot.AppendLine($"[{demo.CurrentGameTick.Value}] Player infos:");
+            SnapshotPlayerInfos();
+
+            demo.CreateTimer(
+                demo.CurrentDemoTick + playerSnapshotInterval,
+                OnSnapshotTimer);
+        }
+
+        demo.CreateTimer(
+            demo.CurrentDemoTick + playerSnapshotInterval,
+            OnSnapshotTimer);
+
+        // Act
+        await demo.Start(GotvCompetitiveProtocol13963, default);
+
+        // Assert
+        Snapshot.Assert(snapshot.ToString());
+    }
+}
