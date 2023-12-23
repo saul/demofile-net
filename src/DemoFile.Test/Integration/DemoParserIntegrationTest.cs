02ad6eeb882256ce7fa0d53c16bee13584903484
Saul Rennison <saul.rennison@gmail.com>
2023-12-23 16:09:20 +0000

    Remove Parse_DemoEvents

--- src/DemoFile.Test/Integration/DemoParserIntegrationTest.cs
+++ src/DemoFile.Test/Integration/DemoParserIntegrationTest.cs
@@ -13,31 +13,6 @@ public class DemoParserIntegrationTest
         await demo.Start(GotvCompetitiveProtocol13963, default);
     }
 
-    [Test]
-    public async Task Parse_DemoEvents()
-    {
-        // Arrange
-        var snapshot = new StringBuilder();
-        var demo = new DemoParser();
-
-        demo.DemoEvents.DemoFileInfo += e =>
-        {
-            snapshot.AppendLine($"[{demo.CurrentDemoTick}/{demo.CurrentGameTick}] TickCount={demo.TickCount}");
-            snapshot.AppendLine($"[{demo.CurrentDemoTick}/{demo.CurrentGameTick}] DemoFileInfo: {JsonSerializer.Serialize(e, DemoJson.SerializerOptions)}");
-        };
-
-        demo.PacketEvents.SvcServerInfo += e =>
-        {
-            snapshot.AppendLine($"[{demo.CurrentDemoTick}/{demo.CurrentGameTick}] SvcServerInfo: {JsonSerializer.Serialize(e, DemoJson.SerializerOptions)}");
-        };
-
-        // Act
-        await demo.Start(GotvCompetitiveProtocol13963, default);
-
-        // Assert
-        Snapshot.Assert(snapshot.ToString());
-    }
-
     private static readonly KeyValuePair<string, Stream>[] CompatibilityCases =
     {
         new("v13978", GotvProtocol13978),
