6ccb956e0c864a2040b0c6ab6b695e3160c0bac0
in0finite <leshow816@gmail.com>
2024-01-15 11:31:16 +0100

    add the rest of files

--- examples/DemoFile.Example.WebAssembly/Program.cs
+++ examples/DemoFile.Example.WebAssembly/Program.cs
@@ -1,2 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text;
+
+namespace DemoFile.Example.WebAssembly;
+
+internal partial class Program
+{
+    public static async Task Main(string[] args)
+    {
+        Console.WriteLine("Main()");
+    }
+
+    [JSExport]
+    internal static async Task<string> ParseToEnd(byte[] buffer)
+    {
+        Console.WriteLine("Parsing started");
+
+        var demo = new DemoParser();
+        using var stream = new MemoryStream(buffer);
+        var sb = new StringBuilder(1024);
+
+        var roundNum = 0;
+        demo.Source1GameEvents.RoundStart += e =>
+        {
+            roundNum += 1;
+            sb.Append("<br />");
+            sb.Append("<br />");
+            sb.AppendLine($"\n\n>>> Round start [{roundNum}] <<<");
+            sb.Append("<br />");
+        };
+
+        demo.Source1GameEvents.RoundFreezeEnd += e =>
+        {
+            sb.Append("<br />");
+            sb.AppendLine("\n  > Round freeze end");
+            sb.Append("<br />");
+        };
+
+        demo.Source1GameEvents.RoundEnd += e =>
+        {
+            sb.Append("<br />");
+            sb.AppendLine("\n  > Round end");
+            sb.Append("<br />");
+        };
+
+        demo.Source1GameEvents.PlayerDeath += e =>
+        {
+            sb.AppendLine($"{e.Attacker?.PlayerName} [{e.Weapon}] -> {e.Player?.PlayerName}");
+            sb.Append("<br />");
+        };
+
+        var sw = Stopwatch.StartNew();
+
+        await demo.ReadAllAsync(stream);
+
+        sb.Append("<br />");
+        sb.AppendLine($"Finished, ticks: {demo.CurrentDemoTick}, elapsed: {sw.ElapsedMilliseconds} ms");
+        sb.Append("<br />");
+
+        return sb.ToString();
+    }
+
+    [JSExport]
+    internal static string Greeting()
+    {
+        return $"Greetings from C#";
+    }
+
+    [JSImport("globalThis.console.log")]
+    internal static partial void JsLog([JSMarshalAs<JSType.String>] string message);
+}
---
+++ examples/DemoFile.Example.WebAssembly/main.js
@@ -0,0 +1,49 @@
+import { dotnet } from './_framework/dotnet.js'
+
+const is_browser = typeof window != "undefined";
+if (!is_browser) throw new Error(`Expected to be running in a browser`);
+
+const { setModuleImports, getAssemblyExports, getConfig } = 
+	await dotnet.create();
+
+setModuleImports("main.js", {
+  
+});
+
+const config = getConfig();
+const exports = await getAssemblyExports(config.mainAssemblyName);
+const text = exports.Program.Greeting();
+console.log(text);
+
+await dotnet.run(); // runs Main() function
+
+
+function openDemoFile()
+{
+	let input = document.createElement('input');
+    input.type = 'file';
+    input.onchange = _ => {
+        let files = Array.from(input.files);
+        console.log(files);
+		readFile(files[0]);
+    };
+    input.click();
+
+    input.remove();
+}
+
+function readFile(file) {
+	document.getElementById("demo_parse_result").innerHTML = "";
+	var reader = new FileReader ();
+	reader.onload = async function (e) {
+		var arrayBuf = reader.result;
+		console.log(arrayBuf.byteLength);
+        var resultStr = await exports.Program.ParseToEnd(new Uint8Array(arrayBuf));
+		document.getElementById("demo_parse_result").innerHTML = resultStr;
+	}
+	reader.readAsArrayBuffer(file);
+}
+
+
+window.document.getElementById("OpenDemoFileButton").onclick = openDemoFile;
+
