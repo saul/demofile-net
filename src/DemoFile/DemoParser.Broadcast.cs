f047f3aaf711d4acbf3d107c0ee476bbd0313e0c
Saul Rennison <saul.rennison@gmail.com>
2024-03-18 22:00:39 +0000

    Re-arrange

--- src/DemoFile/DemoParser.Broadcast.cs
+++ src/DemoFile/DemoParser.Broadcast.cs
@@ -8,101 +8,6 @@ namespace DemoFile;
 
 public partial class DemoParser
 {
-    // Demos tend to start:
-    //
-    // tick         | cmd | command name  | size
-    // [<pre record>] 1 - DemFileHeader - 147 bytes
-    // [<pre record>] 8 - DemSignonPacket - 10 bytes
-    //   - Msg = NetTick, Size = 6
-    // [<pre record>] 8 - DemSignonPacket - 16 bytes
-    //   - Msg = SvcClearAllStringTables, Size = 11
-    //   - Msg = 2, Size = 0
-    //   - Msg = NetNop, Size = 0
-    //   - Msg = NetNop, Size = 0
-    //   - Msg = NetNop, Size = 0
-    // [<pre record>] 8 - DemSignonPacket - 20,793 bytes
-    //   - Msg = SvcCreateStringTable, Size = 35
-    //   - Msg = SvcCreateStringTable, Size = 1410
-    //   - Msg = SvcCreateStringTable, Size = 17464
-    //   - Msg = SvcCreateStringTable, Size = 552
-    //   - Msg = SvcCreateStringTable, Size = 641
-    //   - Msg = SvcCreateStringTable, Size = 51
-    //   - Msg = SvcCreateStringTable, Size = 340
-    //   - Msg = SvcCreateStringTable, Size = 185
-    //   - Msg = SvcCreateStringTable, Size = 28
-    //   - Msg = SvcCreateStringTable, Size = 27
-    //   - Msg = SvcCreateStringTable, Size = 24
-    //   - Msg = NetNop, Size = 0
-    //   - Msg = NetNop, Size = 0
-    //   - Msg = NetNop, Size = 0
-    //   - Msg = NetNop, Size = 0
-    // [<pre record>] 8 - DemSignonPacket - 7,571 bytes compressed
-    //   - Msg = SvcServerInfo, Size = 7795
-    //   - Msg = NetNop, Size = 0
-    // [<pre record>] 8 - DemSignonPacket - 1,368 bytes compressed
-    //   - Msg = NetSetConVar, Size = 1880
-    //   - Msg = NetNop, Size = 0
-    //   - Msg = NetNop, Size = 0
-    //   - Msg = NetNop, Size = 0
-    // [<pre record>] 8 - DemSignonPacket - 10 bytes
-    //   - Msg = NetSignonState, Size = 6
-    // [<pre record>] 4 - DemSendTables - 38,769 bytes compressed
-    // [<pre record>] 5 - DemClassInfo - 3,197 bytes compressed
-    // [<pre record>] 6 - DemStringTables - 18,470 bytes compressed
-    // [<pre record>] 8 - DemSignonPacket - 7 bytes
-    //   - Msg = SvcClassInfo, Size = 2
-    //   - Msg = NetSignonState, Size = 0
-    //   - Msg = NetNop, Size = 0
-    //   - Msg = NetNop, Size = 0
-    //   - Msg = NetNop, Size = 0
-    //   - Msg = NetNop, Size = 0
-    //   - Msg = NetNop, Size = 0
-    // [<pre record>] 8 - DemSignonPacket - 8,645 bytes compressed
-    //   - Msg = SvcVoiceInit, Size = 16
-    //   - Msg = 205, Size = 15397
-    //   - Msg = NetNop, Size = 0
-    //   - Msg = NetNop, Size = 0
-    //   - Msg = NetNop, Size = 0
-    // [<pre record>] 8 - DemSignonPacket - 10 bytes
-    //   - Msg = NetSignonState, Size = 6
-    // [<pre record>] 8 - DemSignonPacket - 10 bytes
-    //   - Msg = NetSignonState, Size = 6
-    // [<pre record>] 3 - DemSyncTick - 0 bytes
-    // [0] 7 - DemPacket - 42,211 bytes compressed
-    //   - Msg = 154, Size = 5
-    //   - Msg = NetTick, Size = 18
-    //   - Msg = SvcPacketEntities, Size = 84404
-
-    // {"tick":52599,"rtdelay":10,"rcvage":0,"fragment":816,"signup_fragment":0,"tps":64,"protocol":5}
-    private class BroadcastSyncDto
-    {
-        [JsonPropertyName("rtdelay")]
-        public double RetransmitDelaySeconds { get; set; }
-
-        [JsonPropertyName("rcvage")]
-        public double ReceiveAgeSeconds { get; set; }
-
-        [JsonPropertyName("fragment")]
-        public int Fragment { get; set; }
-
-        [JsonPropertyName("signup_fragment")]
-        public int SignupFragment { get; set; }
-
-        [JsonPropertyName("tps")]
-        public int TickRate { get; set; }
-
-        [JsonPropertyName("token_redirect")]
-        public string? TokenRedirect { get; set; }
-
-        [JsonPropertyName("protocol")]
-        public int Protocol { get; set; }
-    }
-
-    [JsonSerializable(typeof(BroadcastSyncDto))]
-    private partial class BroadcastJsonSerializerContext : JsonSerializerContext
-    {
-    }
-
     public async Task ReadBroadcastAsync(Uri baseUrl, CancellationToken cancellationToken)
     {
         // todo: take a HTTP client as input
@@ -214,4 +119,33 @@ public partial class DemoParser
 
         return uriBuilder.Uri;
     }
+
+    // {"tick":52599,"rtdelay":10,"rcvage":0,"fragment":816,"signup_fragment":0,"tps":64,"protocol":5}
+    private class BroadcastSyncDto
+    {
+        [JsonPropertyName("rtdelay")]
+        public double RetransmitDelaySeconds { get; set; }
+
+        [JsonPropertyName("rcvage")]
+        public double ReceiveAgeSeconds { get; set; }
+
+        [JsonPropertyName("fragment")]
+        public int Fragment { get; set; }
+
+        [JsonPropertyName("signup_fragment")]
+        public int SignupFragment { get; set; }
+
+        [JsonPropertyName("tps")]
+        public int TickRate { get; set; }
+
+        [JsonPropertyName("token_redirect")]
+        public string? TokenRedirect { get; set; }
+
+        [JsonPropertyName("protocol")]
+        public int Protocol { get; set; }
+    }
+
+    [JsonSerializable(typeof(BroadcastSyncDto))]
+    private partial class BroadcastJsonSerializerContext : JsonSerializerContext
+    {}
 }
